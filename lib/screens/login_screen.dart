88d334eedefed6801cc5625099a1ebd90745040d
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-08-20 10:15:18 +0530

    feat: onboarding screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  static String id = "login_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Login'),
+      ),
+    );
+  }
+}
