a8986edcd727e832a046bba86e158de0a65f7bb8
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-08-20 10:03:03 +0530

    feat: auth bloc

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF16C4B0);
+const kSecondaryColor = Color(0xFFCAF4E5);
+const kBackgroundColor = Color(0xFFFFFFFF);
+const kTextColor = Color(0xFF4D4D4D);
---
+++ lib/utils/config.dart
@@ -0,0 +1,17 @@
+class Config {
+  // user collection name
+  static String fscUsers = "users";
+
+  // user collection fields
+  static String fsfUid = "uid";
+  static String fsfName = "name";
+  static String fsfEmail = "email";
+  static String fsfLoginProvider = "loginProvider";
+
+  // shared prefs key
+  static String prefEmail = "email";
+  static String prefLoggedIn = "loggedIn";
+  static String prefName = "name";
+  static String prefUID = "uid";
+  static String prefProfilePicUrl = "profilePicUrl";
+}
