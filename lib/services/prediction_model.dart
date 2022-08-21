6560ab3034523a216a4b63cff0e8b6af98f3302d
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-08-21 14:19:13 +0530

    feat: prediction api model

---
+++ lib/services/prediction_model.dart
@@ -0,0 +1,48 @@
+import 'package:medibuddy/services/networking.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
+
+const API_URI = 'http://20.198.106.110';
+
+class PredictionModel {
+  Future<dynamic> getHeartPrediction(Object body) async {
+    NetworkHandler networkHelper =
+        NetworkHandler(Uri.parse('$API_URI/predict_heart_disease'), body);
+
+    var predictionData = await networkHelper.getData();
+    return predictionData;
+  }
+
+  Future<dynamic> getBreastPrediction(Object body) async {
+    NetworkHandler networkHelper =
+        NetworkHandler(Uri.parse('$API_URI/predict_breast_cancer'), body);
+
+    var predictionData = await networkHelper.getData();
+    return predictionData;
+  }
+
+  IconData getWeatherIcon(dynamic predictionData) {
+    var id = predictionData['output'];
+    if (id < 30) {
+      return FontAwesomeIcons.faceSmile;
+    } else if (id < 60) {
+      return FontAwesomeIcons.faceFrown;
+    } else if (id <= 100) {
+      return FontAwesomeIcons.faceSadCry;
+    } else {
+      return FontAwesomeIcons.faceTired;
+    }
+  }
+
+  Color getColor(int id) {
+    if (id < 30) {
+      return Colors.green;
+    } else if (id < 60) {
+      return Colors.yellow;
+    } else if (id <= 100) {
+      return Colors.red;
+    } else {
+      return Colors.white;
+    }
+  }
+}
